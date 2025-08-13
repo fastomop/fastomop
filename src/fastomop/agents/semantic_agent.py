d9375f9673e566c086e7962ac8c074c4b8314691
vvcb <vvcb.n1@gmail.com>
2025-08-13 17:57:01 +0100

    Ruff format

--- src/fastomop/agents/semantic_agent.py
+++ src/fastomop/agents/semantic_agent.py
@@ -1,12 +1,10 @@
-
 from fastomop.config import config as cfg
-
 
 
 settings = cfg.semantic_agent
 
 # Not used at the moment, but can be used for A2A integration
-#app = agent.to_a2a(
+# app = agent.to_a2a(
 #    description=cfg.semantic_agent.description,
 #    provider=AgentProvider(organization="FastOMOP Developers", url="example.com"),
-#)
+# )
--- src/fastomop/agents/sql_agent.py
+++ src/fastomop/agents/sql_agent.py
@@ -5,7 +5,7 @@ settings = cfg.sql_agent
 
 
 # Not used at the moment, but can be used for A2A integration
-#app = agent.to_a2a(
+# app = agent.to_a2a(
 #    description=cfg.sql_agent.description,
 #    provider=AgentProvider(organization="FastOMOP Developers", url="example.com"),
-#)
+# )
